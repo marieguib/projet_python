6ac55ca82abbc0c8fb55a56dfe4afd7e613f4e84
Marie Guibert <marieguibert.mg@gmail.com>
2023-02-02 15:24:39 +0100

    Liste astronautes actuels

--- astronautes.py
+++ astronautes.py
@@ -2,4 +2,15 @@ import requests
 
 contenu = requests.get("http://api.open-notify.org/astros.json")
 for personne in contenu.json()["people"]:
-    print(personne)
+    if personne["craft"]=='ISS':
+          print(personne)
+
+# git init : initialisation d'un nouveau dépôt
+# git status : fichier non suivi
+# après git add astronautes.py --> git status : fichier suivi mais pas de commit encore
+# git commit -m"Liste des astronautes dans l'espace"
+# après le commit : git status --> astronautes.py n'apparaît plus
+
+# git log : présente les différents commits
+# informations stockées : 
+# author / date / titre du commit
